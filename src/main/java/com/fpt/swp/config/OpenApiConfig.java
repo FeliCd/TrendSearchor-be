7c0c1cb6996290d5f86a99a15b2cf0d88983a5af
FeliCd <phucnt.sg@gmail.com>
2026-05-14 21:30:40 +0700

    fix: explicit swagger ui server https url

--- src/main/java/com/fpt/swp/config/OpenApiConfig.java
+++ src/main/java/com/fpt/swp/config/OpenApiConfig.java
@@ -5,12 +5,16 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.servers.Server;
+
 @Configuration
 public class OpenApiConfig {
 
         @Bean
         public OpenAPI customOpenAPI() {
                 return new OpenAPI()
+                                .addServersItem(new Server().url("https://trendsearchor-be-production.up.railway.app").description("Production Server"))
+                                .addServersItem(new Server().url("http://localhost:8080").description("Local Server"))
                                 .info(new Info()
                                                 .title("Journal Trend Tracker API")
                                                 .version("1.0")
