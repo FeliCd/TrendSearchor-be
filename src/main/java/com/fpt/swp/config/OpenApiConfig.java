43c2f7ec816a0d2354ee5e03307b5cfd657ccf62
FeliCd <phucnt.sg@gmail.com>
2026-05-14 21:38:02 +0700

    fix: add jwt security scheme to swagger openapi

--- src/main/java/com/fpt/swp/config/OpenApiConfig.java
+++ src/main/java/com/fpt/swp/config/OpenApiConfig.java
@@ -7,17 +7,30 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.servers.Server;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 @Configuration
 public class OpenApiConfig {
 
         @Bean
         public OpenAPI customOpenAPI() {
+                final String securitySchemeName = "bearerAuth";
+                
                 return new OpenAPI()
                                 .addServersItem(new Server().url("https://trendsearchor-be-production.up.railway.app").description("Production Server"))
                                 .addServersItem(new Server().url("http://localhost:8080").description("Local Server"))
                                 .info(new Info()
                                                 .title("Journal Trend Tracker API")
                                                 .version("1.0")
-                                                .description("Hệ thống theo dõi xu hướng báo chí khoa học"));
+                                                .description("Hệ thống theo dõi xu hướng báo chí khoa học"))
+                                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                                .components(new Components()
+                                                .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                                                .name(securitySchemeName)
+                                                                .type(SecurityScheme.Type.HTTP)
+                                                                .scheme("bearer")
+                                                                .bearerFormat("JWT")));
         }
 }
