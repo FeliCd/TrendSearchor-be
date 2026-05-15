ea6c49a902902508d8f3b0363d54a44ae27109f9
FeliCd <phucnt.sg@gmail.com>
2026-05-15 18:20:36 +0700

    feat: fix CORS

--- src/main/java/com/fpt/swp/config/SecurityConfig.java
+++ src/main/java/com/fpt/swp/config/SecurityConfig.java
@@ -81,11 +81,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:5173",
-                "http://localhost:3000",
+        configuration.setAllowedOriginPatterns(Arrays.asList(
+                "http://localhost:*",
                 "https://trendsearchor-be-production.up.railway.app",
-                "https://trend-searchor-fe.vercel.app"
+                "https://trend-searchor-*.vercel.app"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "X-Requested-With"));
